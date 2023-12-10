83406a168e0ebae8553045e33e30c5bc318e9e34
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-12-10 11:04:04 +0000

    Deploying to gh-pages from @ danypryzrak/JSUpgrade-9@388c6e52b66cdb52d4ce8f879f09f84c38b7a225 🚀

---
+++ 01-color-switcher.d71e773d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),n=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.d71e773d.js.map
---
+++ 01-color-switcher.d8ee7a5a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{t.setAttribute("disabled","true"),a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.d8ee7a5a.js.map
